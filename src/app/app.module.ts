1db5be31e63c8bb311d31a025ea620ec24f22d1c
David Ramirez <dramirez2096@gmail.com>
2018-04-26 19:09:59 -0700

    login, nav, and welcome components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavComponent } from './nav/nav.components';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        WelcomeComponent,
+        NavComponent,
+        LoginComponent //telling angular about the component 
     ],
     imports: [
         BrowserModule
     ],
     bootstrap: [
-        AppComponent
+        AppComponent // choosing the file to boot
     ]
 })
 export class AppModule{
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "gw-welcome",
+    templateUrl: "./welcome.component.html",
+    styleUrls: [
+        "./welcome.component.scss"
+    ]
+})
+export class WelcomeComponent{
+    
+}
